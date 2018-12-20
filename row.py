7d5ea11129b74b5c1e22bc2bbf9c327788a60261
Jack Mason <jack.mason@posteo.net>
2018-12-20 14:47:46 +1000

    improved scraper in row.py

---
+++ row.py
@@ -0,0 +1,48 @@
+import urllib.request as req
+from bs4 import BeautifulSoup as soup
+import re
+
+invar = 50
+
+my_url = "https://www.aph.gov.au/Parliamentary_Business/Bills_Legislation/bd/bd1819a"
+base_url = "https://www.aph.gov.au"
+
+#get page
+page = req.urlopen(my_url)
+page_html = page.read()
+page.close()
+
+#html parsing
+page_soup = soup(page_html, "html.parser")
+
+table = page_soup.find(id="libraryDataTable")
+tbody = table.tbody
+
+pat = "tr"
+
+for row in tbody.find_all(pat):
+    text = row.get_text("\n", strip=True) 
+    fields = re.split(re.compile("\n"), text)
+    epub = [link.get('href') for link in row.find_all(href=re.compile("epub"))]
+    pdf = [link.get('href') for link in row.find_all(href=re.compile("pdf"))]
+
+    name, number, date = fields
+    epub_url = re.sub(" ", "%20", base_url+epub[0])
+    pdf_url = pdf[0]
+
+    #print("number: " + number +"\nname: " + name + "\ndate: " + date + "\nepub: " + epub_url + "\npdf:  " + pdf_url)
+    """
+    if False:
+        try:
+            epub_name = str(number) + " "+ name  + " " + "(" + date + ")" + ".epub"
+            req.urlretrieve(epub_url, epub_name)
+        except:
+            print("error")
+    """
+    if True and int(number) > invar:
+        try:
+            epub_name = number + " "+ name  + " " + date + ".epub"
+            req.urlretrieve(epub_url, epub_name)
+        except:
+            print("error")
+
